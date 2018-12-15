2f51813dab77f4680eccdd331781b2832972a253
Tiago Leal <tiago.sleal12@gmail.com>
2018-12-15 09:02:46 -0200

    test the exchange controller

--- spec/requests/exchanges_spec.rb
+++ spec/requests/exchanges_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "Exchanges", type: :request do
   describe "GET #index" do
-    it "works! (now write some real specs)" do
-      get exchanges_index_path
+    it "returns http success" do
+      get '/'
       expect(response).to have_http_status(200)
     end
   end
@@ -13,13 +13,12 @@ RSpec.describe "Exchanges", type: :request do
       @amount = rand(1..9999)
     end
     it 'returns http success' do
-      get '/convert', params:{
+      get '/convert', params: {
                             source_currency: "USD",
                             target_currency: "BRL",
                             amount: @amount
-                      }
-
-      expect(response).to_have_http_status(200)
+                            }
+      expect(response).to have_http_status(200)
     end
   end
 end
